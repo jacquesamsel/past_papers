358424c1cc869e9b9735e2f3106ab2d56bd8a5e3
Jacques Amsel <me@jacquesamsel.com>
2023-08-15 22:12:29 +0200

    Added piwik

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,11 @@
-import Head from 'next/head'
 import '../styles/globals.scss'
+import PiwikProvider from "@piwikpro/next-piwik-pro"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <PiwikProvider
+    containerId='de6cc8be-61b7-4861-b4f2-0af2f34ca6e1'
+    containerUrl='https://jacquesamsel.piwik.pro'
+    ><Component {...pageProps} /></PiwikProvider>
 }
 
 export default MyApp
